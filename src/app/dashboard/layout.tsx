0f81855d3dc002f732fe169047fa6387ed446d40
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2025-03-15 15:23:48 +1100

    fix: fixed deployment issue

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,31 +1,14 @@
-"use client";
-import type React from "react";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../globals.css";
-import { MainNav } from "@/components/main-nav";
-import { Search } from "@/components/search";
-import { UserNav } from "@/components/user-nav";
+// app/dashboard/layout.tsx
 import { ThemeProvider } from "@/components/theme_provider";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      {children}
+    </ThemeProvider>
   );
 }
