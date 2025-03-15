273b13810a72e788d3ddfb6e67ea7d7f7ac92728
darrenmvsn <darrenmelvinson@gmail.com>
2025-03-15 13:41:14 +1100

    made users and transactions functions

---
+++ src/lib/user.ts
@@ -0,0 +1,30 @@
+const { userCollection } = require('./db');
+
+interface User {
+    _id: string;
+    profilePicture: string;
+    username: string;
+    email: string;
+    passwordHash: string;
+}
+
+export async function createUser(user: User) {
+    try {
+        const result = await userCollection.insertOne(user);
+        return result.insertedId;
+    } catch (error) {
+        console.error('Error creating user:', error);
+        throw error;
+    }
+}
+
+export async function getUserById(id: string) {
+    try {
+        const result = await userCollection.findOne({ _id: id });
+        return result as User | null;
+    } catch (error) {
+        console.error(`Error fetching user ${id}:`, error);
+        throw error;
+    }
+}
+
