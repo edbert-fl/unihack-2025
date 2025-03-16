4d8952080c558efd68f78b52cd3316d2d1844a12
zyler-tzt <thantzin.tzh14@gmail.com>
2025-03-16 12:09:03 +1100

    Fix search params

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,21 +1,30 @@
 "use client"
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { WalletAddressInfo } from "@/components/wallet/wallet-address-info"
+import { WalletAddressInfo } from "@/components/wallet/wallet-address-info";
 
-export default function Home() {
+function WalletComponent() {
     const searchParams = useSearchParams();
-    const [walletAddress, setWalletAddress] = useState<string>("")
+    const [walletAddress, setWalletAddress] = useState<string>("");
+
     useEffect(() => {
         const id = searchParams.get("address");
         if (id) {
             setWalletAddress(id);
         }
-    }, [searchParams])
+    }, [searchParams]);
+
+    return <WalletAddressInfo walletAddress={walletAddress} />;
+}
+
+export default function Home() {
     return (
-    <main className="min-h-screen bg-gradient-to-b from-black via-slate-900 to-black text-white p-4 md:p-6 lg:p-8">
-        <WalletAddressInfo walletAddress={walletAddress} />
-    </main>
-    )
+        <main className="min-h-screen bg-gradient-to-b from-black via-slate-900 to-black text-white p-4 md:p-6 lg:p-8">
+            <Suspense fallback={<div>Loading...</div>}>
+                <WalletComponent />
+            </Suspense>
+        </main>
+    );
 }
