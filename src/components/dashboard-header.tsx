32fde1a332cbdc8949f8efc721fa305fea1fef3f
darrenmvsn <darrenmelvinson@gmail.com>
2025-03-16 14:09:12 +1100

    updated the dashboard page

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -4,7 +4,7 @@ import { CalendarIcon, Download } from "lucide-react";
 export function DashboardHeader() {
   return (
     <div className="flex items-center justify-between space-y-2">
-      <h2 className="text-3xl font-bold tracking-tight">RedCross Dashboard</h2>
+      <h2 className="text-3xl font-bold tracking-tight">Purple Cross Dashboard</h2>
       <div className="flex items-center space-x-2">
         <Button className="bg-gradient-to-r from-[#4169e1] to-[#9333ea] ">
           <Download className="mr-2 h-4 w-4 text-white" />
